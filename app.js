0ef4894e018f50c8b5a94dd38d8e75511a8ff500
ccottier <chrystiecottier@gmail.com>
2017-02-11 17:40:47 -0800

    got data to populate

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //Global Variables estab//
 var pikePlace = {
   min: 17,
@@ -8,31 +7,42 @@ var pikePlace = {
   hourOfDay: ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm','5pm'],
 
   // random customer per hour number generator
-  getRandom: function (min, max) {
+  getRandom: function(min, max) {
     //could also use: return Math.floor(Math.random() * (max - min + 1)) + min;
-    return Math.round(Math.round) * (max - min + 1) + min;
+    return Math.round((Math.random() * (this.max - this.min + 1) + this.min));
   },
 
   cookiesPerHour: [],
-  totalCookies = 0,
+  totalCookies: 0,
   // cookiesPerHour generator
   populate: function() {
-    for(var i=0; i < hourOfDay.length, i++) {
-      var randomNum = this.getRandom ();
+    for(i = 0; i < this.hourOfDay.length; i++) {
+      var randomNum = this.getRandom();
       this.cookiesPerHour.push(randomNum);
-      this.totalCookies += rand;
+      this.totalCookies += this.rand;
+    }
+  },
+  //call function to generate cookie totals for Pike Place
+
+
+  render: function() {
+    this.populate()
+    for (i = 0; i < this.hourOfDay.length; i++) {
+      var pikePlaceEl = document.getElementById('pikePlace');
+      var liRandomNumEl = document.createElement('li');
+      console.log(liRandomNumEl);
+      liRandomNumEl.textContent = this.hourOfDay[i] + ':' + ' ' + this.cookiesPerHour[i];
+      pikePlaceEl.appendChild(liRandomNumEl);
     }
   }
 };
-//call function to generate cookie totals for Pike Place
-pikePlace.generateCookies();
 
-var parent = document.getElementById('pikePlace');
-var child;
-var
-    // render your object data to the DOM as an unordered list of data points!!
-    // 1. Get an element by Id from your html
-    // 2. Create a UL element
-    // 3. Create and populate an LI element for each of the data points that you want to render
-    // 4. Append the LI to the UL
-    // 5. Append the UL to the element that you stored in step 1
+pikePlace.render()
+// var child;
+// var
+// render your object data to the DOM as an unordered list of data points!!
+// 1. Get an element by Id from your html
+// 2. Create a UL element
+// 3. Create and populate an LI element for each of the data points that you want to render
+// 4. Append the LI to the UL
+// 5. Append the UL to the element that you stored in step 1
